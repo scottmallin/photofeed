6069fe7cfc86f82508c8ef08dcf264282e8e1913
Scott Mallin <scott@mallin.co>
2022-03-24 21:23:03 +0000

    project so far

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -56,6 +57,12 @@ class AuthController extends Controller
         return response($response, 200);
     }
 
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
 
